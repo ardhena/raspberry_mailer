543956b75d7bfa033d50566f04b2fa31f77234a6
Olga Draber <olga.draber@netguru.pl>
2015-12-20 11:25:07 +0100

    refactor file loader - simplification, load only specified extenstion files from dirs

--- config/file_loader.rb
+++ config/file_loader.rb
@@ -1,8 +1,8 @@
 module Config
   class FileLoader
-    LOADED_FILES = [ 'app.rb', 'file_loader.rb']
-    EXCLUDED_DIRS = [ '.', '..', '.git']
-    EXCLUDED_FILES = [ 'log', 'Gemfile.lock', LOADED_FILES ].flatten
+    LOAD_DIR = 'app'
+    LOAD_FILE_EXTENSTIONS = [ '.rb' ]
+    EXCLUDE_DIRS = [ '.', '..' ]
 
     attr_reader :path
 
@@ -10,44 +10,25 @@ module Config
       @path = path
     end
 
+    def gems
+      fetch_gems
+    end
+
+    def files
+      fetch_files("#{path}/#{LOAD_DIR}").reject(&:nil?).sort
+    end
+
     def call
-      files_to_load.each do |file|
+      (files + gems).each do |file|
         require file
       end
     end
 
     private
 
-    def files_to_load
-      generate_files(path).reject(&:nil?).sort
-    end
-
-    def generate_files(path)
-      Dir.entries(path).flat_map do |entry|
-        if directory?(path, entry)
-          generate_files(file_with_path(path, entry))
-        elsif file?(path, entry)
-          add_entry(path, entry)
-        end
-      end
-    end
-
-    def directory?(path, entry)
-      File.directory?(file_with_path(path, entry)) && !EXCLUDED_DIRS.include?(entry)
-    end
-
-    def file?(path, entry)
-      File.file?(file_with_path(path, entry)) && !EXCLUDED_FILES.include?(entry)
-    end
-
-    def add_entry(path, entry)
-      return generate_gems(path, entry) if entry == 'Gemfile'
-      file_with_path(path, entry)
-    end
-
-    def generate_gems(path, entry)
+    def fetch_gems
       gems = []
-      File.foreach(file_with_path(path, entry)) do |line|
+      File.foreach("#{path}/Gemfile") do |line|
         if line.include?('gem') && !line.include?('source')
           gems << gem_name(line)
         end
@@ -55,12 +36,27 @@ module Config
       gems
     end
 
-    def gem_name(line)
-      line.split("'").reject{ |line| line == 'gem ' }.first
+    def fetch_files(sub_path)
+      Dir.entries(sub_path).flat_map do |entry|
+        file = "#{sub_path}/#{entry}"
+        if directory?(file, entry)
+          fetch_files(file)
+        elsif file?(file)
+          file
+        end
+      end
     end
 
-    def file_with_path(path, file)
-      "#{path}/#{file}"
+    def directory?(file_name, entry)
+      File.directory?(file_name) && !EXCLUDE_DIRS.include?(entry)
+    end
+
+    def file?(file_name)
+      File.file?(file_name) && LOAD_FILE_EXTENSTIONS.include?(File.extname(file_name))
+    end
+
+    def gem_name(line)
+      line.split("'").reject{ |line| line == 'gem ' }.first
     end
   end
 end
