70a526d726b228528e7fd1b206adb2b2b99f1571
olgad <olgadraber@gmail.com>
2016-02-13 13:12:14 +0100

    deploy with deployer, bundle install without test

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,13 +1,13 @@
 lock '3.4.0'
 
 set :application, 'raspberry_mailer'
-set :repo_url, 'olgad@192.168.0.106:/home/olgad/git/raspberry_mailer.git'
+set :repo_url, 'deployer@192.168.0.106:/home/olgad/git/raspberry_mailer.git'
 set :pty, true
 
 namespace :setup do
   task :bundle do
     on roles(:app) do
-      execute "cd #{current_path} && /home/olgad/.rvm/gems/ruby-2.2.4/wrappers/bundle install --without deployment"
+      execute "cd #{current_path} && /home/olgad/.rvm/gems/ruby-2.2.4/wrappers/bundle install --without deployment test"
     end
   end
 end
--- config/deploy/raspberry.rb
+++ config/deploy/raspberry.rb
@@ -1,3 +1,3 @@
-server '192.168.0.106', user: 'olgad', roles: %w{app}
+server '192.168.0.106', user: 'deployer', roles: %w{app}
 
 after 'deploy', 'setup:bundle'
