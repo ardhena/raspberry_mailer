4bde6da29c405fe647ab1a6bf462c5f8cd445740
olgad <olgadraber@gmail.com>
2016-02-21 21:38:04 +0100

    add time table template and mailer

--- app/mailers/time_table.rb
+++ app/mailers/time_table.rb
@@ -1,9 +1,27 @@
 module Mailer
   class TimeTable
-    attr_reader :template_name
+    attr_reader :saturday, :sunday, :template_path, :fetcher
 
-    def initialize
-      @template_name = self.class.name.split('::').last.snake_case
+    TEMPLATE_NAME = 'time_table'
+
+    def initialize(date)
+      @saturday = date.next.next.next
+      @sunday = saturday.next
+      @template_path = "#{::PATH}/app/templates/#{TEMPLATE_NAME}.html.erb"
+    end
+
+    def call
+      fetch_data
+      MailSender.new(@subject, @template).call
+    end
+
+    private
+
+    def fetch_data
+      @saturday_classes = Formatter::TimeTable.new(saturday).call
+      @sunday_classes = Formatter::TimeTable.new(sunday).call
+      @template = ERB.new(File.read(template_path)).result(binding)
+      @subject = "Plan zajęć UAM - #{saturday.strftime('%d.%m.%Y')}, #{sunday.strftime('%d.%m.%Y')}"
     end
   end
 end
---
+++ test/mailers/time_table_test.rb
@@ -0,0 +1,22 @@
+require 'test_helper'
+require_relative '../../app/mailers/time_table'
+
+require 'date'
+require_relative '../../config/initializers/string'
+
+PATH = 'test'
+
+module Mailer
+  class TimeTableTest < Minitest::Test
+    def setup
+      @date = Date.parse('2016-01-09')
+      @fetcher = TimeTable.new(@date)
+    end
+
+    def test_initialize
+      assert_equal Date.parse('2016-01-12'), @fetcher.saturday
+      assert_equal Date.parse('2016-01-13'), @fetcher.sunday
+      assert_equal 'test/app/templates/time_table.html.erb', @fetcher.template_path
+    end
+  end
+end
